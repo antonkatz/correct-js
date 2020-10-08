dbb0d03358327f088ba32c43686ff1c41aca87be
Anton Kats <antonkats@gmail.com>
2020-10-07 23:42:48 -0300

    Improved hacky types

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,17 +1,21 @@
-export type Struct<C extends {}, Ops extends {}> = Partial<C> & Ops & {
+export type Struct<C extends {}, Ops extends {[k: string]: (this: Struct<C, Ops>, ...rest: any[] | undefined) => any}> = C & Ops & {
     Contents: Partial<C>
 }
 
-export interface Factory<C, Ops> {
-    (contents: Partial<C>): Partial<C> & Ops
+// type Initializer<This> =
+
+export interface Factory<C, Ops extends {[k: string]: (this: Struct<C, Ops>, ...rest: any[] | undefined) => any}, Ires> {
+    (contents: Partial<C>): Ires extends Promise<any> ? Promise<Struct<C, Ops> | undefined> : (Struct<C, Ops> | undefined)
     fromContents(contents: {}): Partial<C> & Ops
-    mix<addC, addOps>(defaultContents: {}, operators: {}, initializer: (this: Struct<C, Ops>) => any): Factory<C & addC, Ops & addOps>
+
+    // fixme. addIres in return should be conditional, etc
+    mix<addC, addOps, addIres>(defaultContents: {}, operators: {}, initializer: (this: Struct<C, Ops>) => addIres): Factory<C & addC, Ops & addOps, addIres>
 }
 
-export function buildFactory<C extends {}, Ops extends {[k: string]: (this: Struct<C, Ops>) => any}>(
+export function buildFactory<C extends {}, Ops extends {[k: string]: (this: Struct<C, Ops>, ...rest: any[] | undefined) => any}, Ires extends (any | Promise<any>)>(
     contents: C,
     operations: Ops,
-    initializer: (this: Partial<C> & Ops) => any
-): Factory<C, Ops>
+    initializer: (this: Struct<C, Ops>) => Ires
+): Factory<C, Ops, Ires>
 
 export function buildStruct()
